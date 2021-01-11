c7cd5c5476bea1c3b257401574f613958ef3d12d
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2021-01-11 17:19:05 -0300

    use only the provided asset

--- fetch_bbb_video.sh
+++ fetch_bbb_video.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
-read -p "WARNING: You're about to download 340MB, are you sure? (yN) " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-  echo "n"
-  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-fi
+#  the link doesn't work anymore
+# wget -O bunny_1080p_60fps.mp4 http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_normal.mp4
+# ffmpeg -y -i bunny_1080p_60fps.mp4 -ss 00:01:24 -t 00:00:10 small_bunny_1080p_60fps.mp4
 
-check_cmd()
-{
-  if ! which $1 &>/dev/null; then
-    error "$1 command not found, you must install it before."
-  fi
-}
-
-check_cmd wget
-check_cmd ffmpeg
-
-wget -O bunny_1080p_60fps.mp4 https://github.com/leandromoreira/big-buck-bunny/raw/main/Big-Buck-Bunny-1080p-h264-4mb-60fps-multi-ch-ac3.mp4
-ffmpeg -y -i bunny_1080p_60fps.mp4 -ss 00:01:24 -t 00:00:10 small_bunny_1080p_60fps.mp4
+echo "the small_bunny_1080p_60fps.mp4 is already provided"
